5956b243657f10975c23ec498e3b5aacabf63ec8
Evgeniy.S <evgenicuss@gmail.com>
2020-12-20 15:28:17 +0300

    new

---
+++ main.js
@@ -0,0 +1,49 @@
+function fff() {
+	function fff() {
+		function fff() {
+			function fff() {
+				function fff() {
+
+				}
+			}
+		}
+	}
+	console.log();
+	let ddd = 1;
+
+	function fff() {
+		function fff() {
+			function fff() {
+				function fff() {
+
+				}
+			}
+		}
+	}
+	console.log();
+	let ddd = 1;
+
+	function fff() {
+		function fff() {
+			function fff() {
+				function fff() {
+					function fff() {
+						function fff() {
+							function fff() {
+								function fff() {
+
+								}
+
+							}
+						}
+					}
+					console.log();
+					let ddd = 1;
+				}
+			}
+		}
+	}
+	console.log();
+	let ddd = 1;
+
+}
